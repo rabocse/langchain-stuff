09fe05ca1b51dded328f99bc669b210ff953d9db
Alex Rabocse <alexdescobars@gmail.com>
2024-02-24 22:41:28 +0000

    Works but the output is not formatted

--- 2.IceBreaker.py
+++ 2.IceBreaker.py
@@ -7,37 +7,38 @@ from third_party.linkedin import scrape_linkedin_profile
 
 
 if __name__ == "__main__":
-    #load_dotenv()
+##    load_dotenv()
 
-    # env_variable_value = os.getenv('OPENAI_API_KEY')
-
-    # information = """
-    #     The person needs a device that can offer network perimeter security. 
-    #     It needs to run in routed mode and provide stateful packet filtering.
-    #     Layer 7 inspections are a must.
-    #     It should support some kind of centralized management since the person needs to manage multiple branches.
-    # """
-
-
-    # ## Prompt to be used in the Prompt Template
-    # summary_template = """
-    # Given the Linkedin information {information} about a requirement from a network engineer, I want you to create:
-
-    # 1. A short summary of a Cisco solution that could help for such requirement.
-    # 2. Additional features (two or three is fine) from the chosen Cisco solution.
-    # """
-
-    # summary_prompt_template = PromptTemplate(input_variables=["information"], template= summary_template)
+    env_variable_value = os.getenv('OPENAI_API_KEY')
 
 
 
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    ## Prompt to be used in the Prompt Template
+    summary_template = """
+    Given the Linkedin information {information} (the information is in JSON format) about a person, I want you to create:
 
-    # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    # res = chain.invoke(input={"information": information})
+    1. A title in capital. 
+    2. A hort summary.
+    3. Two interesting facts about the person. 
+    """
 
-    # print(res)
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template= summary_template)
 
-    linkedin_data= scrape_linkedin_profile(linkedin_profile_url='https://gist.githubusercontent.com/rabocse/6cf6dc6808f5abf983acdb4e5f6dba03/raw/40041968fc633f3a38a5ea95155ffa5f01a63eb6/scrapedLinkedinProfile.json')
+    print(" ==================================\n")
 
-    print(linkedin_data.json())
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    linkedin_data= scrape_linkedin_profile(linkedin_profile_url='https://gist.githubusercontent.com/rabocse/6cf6dc6808f5abf983acdb4e5f6dba03/raw/3b8132051891134ce4e4cfc97912d1ee107f9463/scrapedLinkedinProfile.json')
+
+    ## res = chain.invoke(information=linkedin_data.json())
+    data =str(linkedin_data.json())    
+
+    print(" ==================================\n")
+
+    res = chain.invoke(input={"information":data})
+    
+    print(res)
+
+    print(" ==================================\n")
