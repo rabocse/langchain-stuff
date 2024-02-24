16c0f9eaa73bfa60ded18fb6bac51cf1b714f88d
Alex Rabocse <alexdescobars@gmail.com>
2024-02-24 22:40:52 +0000

    Commented out the key since the webpage does not require authentication

--- third_party/linkedin.py
+++ third_party/linkedin.py
@@ -6,7 +6,7 @@ def scrape_linkedin_profile(linkedin_profile_url: str):
     """Scrape information from Linkedin profiles
     Manually scrape the information from the Linkedin profile"""
     api_endpoint= linkedin_profile_url
-    header_dic = {"Authorization": f'Bearer {os.environ.get("API_KEY_FOR_ENDPOINT")}'}
+   # header_dic = {"Authorization": f'Bearer {os.environ.get("API_KEY_FOR_ENDPOINT")}'}
 
   
     response= requests.get(api_endpoint)
